4bd578d6c7203f089aa3091edf5a8dc1b7674611
malxcolm <89610579+malxcolm@users.noreply.github.com>
2021-10-16 13:17:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,16 @@ import os
 
 print("HackerOS v2")
 print("Version 2.00...")
-print("Wanna Start the Setup? y/n")
-      input("")
-
+print("Starting Setup Now")
+print("connecting to https://github.com/malxcolm/OSfiles.py")
 
 #Loading with spice
-for i in tqdm (range (1000000000000009009009000000000000000000000000000), 
-               desc="Setting Up: system12.py and hackeros.py", 
+for i in tqdm (range (10000000), 
+    
+             desc="Setting Up Operating System", 
                ascii=False, ncols=75):
-    time.sleep(0.01)
+    time.sleep(0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000001)
       
 print("Downloaded System Files")
 print("Setting Up digital harddrive")
-      for i in tqdm (range (1000000000000009009009000000000000000000000000000000), 
-               desc="harddrive.py is created. creating 20.01GB -i", 
-               ascii=False, ncols=75):
-    time.sleep(0.01)
-      
+     
